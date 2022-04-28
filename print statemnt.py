ee15f75dd71427144a0ac784f702e1e187beac9c
Bhargavimadipoju <104247048+Bhargavimadipoju@users.noreply.github.com>
2022-04-28 14:13:15 +0530

    Add files via upload

---
+++ print statemnt.py
@@ -0,0 +1,14 @@
+"""date:19-04-2022 TUSEDAY
+Name:Bhargavi
+file desc: print statement
+"""
+a=4
+print(a)#print int
+b=9.0
+print(b)#print int
+c=67
+print(c)#print float
+d="bhargavi"
+print(d)#print string
+
+
